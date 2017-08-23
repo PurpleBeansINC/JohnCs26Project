aa4d6d616184c9da2b252928953dfd5c609ffe53
John Neijzen <john.neijzen@live.com>
2017-08-24 04:28:52 +0800

    Clean UP

--- JohnCs26Project/MainWindow.xaml.cs
+++ JohnCs26Project/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using System;
-using System.Drawing;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace JohnCs26Project
 {
@@ -21,8 +9,8 @@ namespace JohnCs26Project
     /// </summary>
     public partial class MainWindow : Window
     {
-        int NumberOfPeople = 0;
-        int state = 0; // 0 = empty, 1 = woman, 2 = man
+        private int NumberOfPeople = 0;
+        private int state = 0; // 0 = empty, 1 = woman, 2 = man
 
         public MainWindow()
         {
@@ -32,7 +20,8 @@ namespace JohnCs26Project
 
         public void woman_wants_to_enter()
         {
-            if ((state == 0) || (state == 1)) {
+            if ((state == 0) || (state == 1))
+            {
                 state = 1;
                 NumberOfPeople++;
             }
@@ -51,10 +40,10 @@ namespace JohnCs26Project
 
         public void woman_leaves()
         {
-            if ((state == 1) && (NumberOfPeople !=0 ))
+            if ((state == 1) && (NumberOfPeople != 0))
             {
                 NumberOfPeople--;
-                if(NumberOfPeople == 0)
+                if (NumberOfPeople == 0)
                 {
                     state = 0;
                 }
@@ -83,7 +72,7 @@ namespace JohnCs26Project
             {
                 doorImage.Source = new BitmapImage(new Uri(@"Assest/Empty Door.png", UriKind.Relative));
             }
-            else if(state == 1)
+            else if (state == 1)
             {
                 doorImage.Source = new BitmapImage(new Uri(@"Assest/Woman Door.png", UriKind.Relative));
             }
